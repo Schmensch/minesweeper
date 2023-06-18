1db86b167d731379fb07cc65a99a9e99ce871849
IndexHD <ludwigindexhd@richter.ist>
2023-06-18 22:11:31 +0200

    addet basic menu + some gitignore

--- cpp/minesweeper.cpp
+++ cpp/minesweeper.cpp
@@ -7,18 +7,65 @@ int main()
 {
     bool gameloop = true;
     while (gameloop) {
-        cout << "test" << endl;
+        cout << "Welcome to MINESWEEPER \n\nPlease selekt a difficulty: \n\t Normal [N] \n\t Hard [H] \n\t Expert [E]" << endl;
+        char difin;
+        int x;
+        int y;
+        int nmins;
+        int dif = -11;
+
+        //sets x,z and nmins acording to player input
+        while (dif == -11){
+            cin >> difin;
+            difin = tolower(difin);
+            switch (difin){
+                case 'n':
+                    dif = 0;
+                    x = y = 7;
+                    nmins = 9;
+                    break;
+                case 'h':
+                    dif = 1;
+                    x = y = 15;
+                    nmins = 39;
+                    break;
+                case 'e':
+                    dif = 2;
+                    x = 15;
+                    y = 29;
+                    nmins = 98;
+                    break;
+                default:
+                    cout << "Enter valid answer!" << endl;
+                    break;
+            }
+        }
+
+
+        /*
+        still figuring shit out
+
+        int board [y][x] = {
+            {1,0,0,0},
+            {0,0,0,0}
+        };
+
+        for (int i=0; i>7; i++) {
+            cout << board [0][0];
+        }
+        */
+        
 
 
         //quits/restarts Gameloop
         while (true) {
-            cout << "Play Again? \b Y/N" << endl;
+            cout << "Play Again? \nY/N" << endl;
             char input;
             cin >> input;
-            input = toupper(input);
-            if (input == 'Y') {
+            input = tolower(input);
+            if (input == 'y') {
                 break;
-            } else if (input == 'N') {
+            } else if (input == 'n') {
                 return 0;
             } else {
                 cout << "Enter valid answer!" << endl;
