0e5f7e7d614e5b7cee4e86f234e4722323195764
Jonathan Zeller <git@jonathanzeller.de>
2023-06-21 18:41:35 +0200

    properly zero out board and flags

--- cpp/minesweeper.cpp
+++ cpp/minesweeper.cpp
@@ -3,85 +3,101 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    //main gameloop
+    // main gameloop
     bool gameloop = true;
-    while (gameloop) {
+    while (gameloop)
+    {
 
-        //anounces game
+        // anounces game
         cout << "Welcome to MINESWEEPER \n\nPlease selekt a difficulty: \n\t Normal [N] \n\t Hard [H] \n\t Expert [E]" << endl;
         char difin;
         int boardX, boardY, nmins, dif = -1;
 
-        //sets boardX, boardY and nmins acording to player input
-        while (dif == -1){
+        // sets boardX, boardY and nmins acording to player input
+        while (dif == -1)
+        {
             cin >> difin;
             difin = tolower(difin);
-            switch (difin){
-                case 'n':
-                    dif = 0;
-                    boardX = boardY = 8;
-                    nmins = 10;
-                    break;
-                case 'h':
-                    dif = 1;
-                    boardX = boardY = 16;
-                    nmins = 40;
-                    break;
-                case 'e':
-                    dif = 2;
-                    boardX = 16;
-                    boardY = 30;
-                    nmins = 99;
-                    break;
-                default:
-                    cout << "Enter valid answer!" << endl;
-                    break;
+            switch (difin)
+            {
+            case 'n':
+                dif = 0;
+                boardX = boardY = 8;
+                nmins = 10;
+                break;
+            case 'h':
+                dif = 1;
+                boardX = boardY = 16;
+                nmins = 40;
+                break;
+            case 'e':
+                dif = 2;
+                boardX = 16;
+                boardY = 30;
+                nmins = 99;
+                break;
+            default:
+                cout << "Enter valid answer!" << endl;
+                break;
             }
         }
 
-        //creats board
-        int board [boardX] [boardY] = {0};
-        bool mask [boardX] [boardY] = {true};
+        // creats board
+        int board[boardX][boardY] = {0};
+        bool maskVisible[boardX][boardY] = {0};
 
-        //board output
-        for(int i = 0; i< boardX; i++) {
-            cout << "-";
-            for (int k = 0; k < boardY; k++) {
-                cout << "----";
+        // Zero the board
+        for (int i = 0; i < boardX; i++)
+        {
+            for (int j = 0; j < boardY; j++)
+            {
+                board[i][j] = 0;
+                maskVisible[i][j] = false;
+            }
+        }
+
+        // debug board output
+        for (int i = 0; i < boardX; i++)
+        {
+            for (int j = 0; j < boardY; j++)
+            {
+                cout << board[i][j] << " ";
             }
             cout << endl;
-            for (int j = 0; j < boardY; j++) {
-                cout << "|";
-                if (mask [i] [j]) {
-                    cout << " " << board [i] [j] << " ";
-                } else {
-                    cout << " 9 ";
-                }
+        }
+
+        cout << "\n\n\n";
+
+        // debug visible maskVisible output
+        for (int i = 0; i < boardX; i++)
+        {
+            for (int j = 0; j < boardY; j++)
+            {
+                cout << maskVisible[i][j] << " ";
             }
-        cout << "|";
-        cout <<endl;
+            cout << endl;
         }
-        cout << "-";
-        for (int k = 0; k < boardY; k++) {
-            cout << "----";
-        }
-        cout << endl;
-    
 
-
-        //quits/restarts Gameloop
-        while (true) {
+        // quits/restarts Gameloop
+        while (true)
+        {
             cout << "Play Again? \nY/N" << endl;
             char input;
             cin >> input;
             input = tolower(input);
-            if (input == 'y') {
+            if (input == 'y')
+            {
                 break;
-            } else if (input == 'n') {
+            }
+            else if (input == 'n')
+            {
                 return 0;
-            } else {
+            }
+            else
+            {
                 cout << "Enter valid answer!" << endl;
             }
         }
@@ -89,11 +105,10 @@ int main(){
     return 0;
 }
 
-
 /*
 for (int i = 0; i < 9; i++) {
     for (int j = 0; j < 9; j++) {
-        if (mask [i] [j] == false ) {
+        if (maskVisible [i] [j] == false ) {
             cout << "true" << endl;
         }
     }
