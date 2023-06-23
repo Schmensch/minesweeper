df0926aacd225e7566c8fee519df0f04f7b369a7
Jonathan Zeller <git@jonathanzeller.de>
2023-06-24 00:18:32 +0200

    automatic formatting

--- cpp/minesweeper.cpp
+++ cpp/minesweeper.cpp
@@ -5,62 +5,82 @@
 
 using namespace std;
 
-//Prints board (set maskVisible to !maskVisible for debuging)
+// Prints board (set maskVisible to !maskVisible for debuging)
 void printBoard(vector<vector<int>> board, vector<vector<bool>> maskVisible)
 {
     for (int i = 0; i < board.size(); i++)
     {
         cout << "-";
-        for (int k = 0; k < board[i].size(); k++) {
+        for (int k = 0; k < board[i].size(); k++)
+        {
             cout << "----";
         }
-        cout << endl << "|";
+        cout << endl
+             << "|";
         for (int j = 0; j < board[i].size(); j++)
         {
-            if (maskVisible[i][j]) {
-                if (board[i][j] < 0){
+            if (maskVisible[i][j])
+            {
+                if (board[i][j] < 0)
+                {
                     cout << " " << board[i][j] << "|";
-                } else {
-                cout << " " << board[i][j] << " |";
                 }
-            } else {
+                else
+                {
+                    cout << " " << board[i][j] << " |";
+                }
+            }
+            else
+            {
                 cout << "   |";
             }
         }
         cout << "\n";
     }
     cout << "-";
-    for (int k = 0; k < board[0].size(); k++) {
+    for (int k = 0; k < board[0].size(); k++)
+    {
         cout << "----";
     }
     cout << endl;
 }
 
-//Checks if Koordiante is on board
-bool isValid(vector<int> toCheck, int boardSizeX, int boardSizeY) {
+// Checks if Koordiante is on board
+bool isValid(vector<int> toCheck, int boardSizeX, int boardSizeY)
+{
     bool isValid;
-    if (toCheck[0] <= boardSizeX) {
-        if (toCheck[0] >0){
+    if (toCheck[0] <= boardSizeX)
+    {
+        if (toCheck[0] > 0)
+        {
             isValid = true;
-            if (toCheck[1] <= boardSizeY) {
-                if (toCheck[1] >0){
+            if (toCheck[1] <= boardSizeY)
+            {
+                if (toCheck[1] > 0)
+                {
                     isValid = true;
                 }
-            } else {
-        isValid = false;
-    }
+            }
+            else
+            {
+                isValid = false;
+            }
         }
-    } else {
+    }
+    else
+    {
         isValid = false;
     }
     return isValid;
 }
 
-//Takes user input
-vector<int> userInput(int boardSizeX, int boardSizeY) {
-    vector<int> userInput = {0,0};
+// Takes user input
+vector<int> userInput(int boardSizeX, int boardSizeY)
+{
+    vector<int> userInput = {0, 0};
     int input;
-    while (!isValid(userInput, boardSizeX, boardSizeY)){
+    while (!isValid(userInput, boardSizeX, boardSizeY))
+    {
         cout << "Pleas enter a koordinate \nEnter X:";
         cin >> input;
         userInput.at(0) = input;
@@ -71,14 +91,15 @@ vector<int> userInput(int boardSizeX, int boardSizeY) {
     return userInput;
 }
 
-//Generates random Koordinate using <random>
-vector<int> randomKoordinate (int boardSizeX, int boardSizeY) {
+// Generates random Koordinate using <random>
+vector<int> randomKoordinate(int boardSizeX, int boardSizeY)
+{
     vector<int> random;
     random_device rand;
-    mt19937 gen(rand());  
-    uniform_int_distribution<>dis(1, boardSizeX);
+    mt19937 gen(rand());
+    uniform_int_distribution<> dis(1, boardSizeX);
     random.push_back(dis(gen));
-    uniform_int_distribution<>cis(1, boardSizeY);
+    uniform_int_distribution<> cis(1, boardSizeY);
     random.push_back(cis(gen));
     return random;
 }
@@ -144,24 +165,30 @@ int main()
         }
     }
 
-    //First move
+    // First move
     int moves = 1;
     printBoard(board, maskVisible);
     cout << "make first move \n";
     vector<int> firstmMove = userInput(boardSizeX, boardSizeY);
 
-    //Generate mines (replace 'randomKoordinate' with 'userInput' to generate mines yourself(only recomendet on easy))
-    for (int i = 0; i < numMines; i++) {
+    // Generate mines (replace 'randomKoordinate' with 'userInput' to generate mines yourself(only recomendet on easy))
+    for (int i = 0; i < numMines; i++)
+    {
         vector<int> koordinateMine = randomKoordinate(boardSizeX, boardSizeY);
-        //cout << koordinateMine[0] << " " << koordinateMine[1] << endl;
+        // cout << koordinateMine[0] << " " << koordinateMine[1] << endl;
         int i1 = --koordinateMine[0], i2 = --koordinateMine[1];
-        if (board[i1][i2] < 0) {
+        if (board[i1][i2] < 0)
+        {
             i--;
-        } else if (firstmMove == koordinateMine) {
+        }
+        else if (firstmMove == koordinateMine)
+        {
             i--;
-        } else {
+        }
+        else
+        {
             board[koordinateMine[0]][koordinateMine[1]] = -1;
-            //cout << "mine placed succesfully\n";
+            // cout << "mine placed succesfully\n";
         }
     }
 
